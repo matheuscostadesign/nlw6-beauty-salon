b7b0c6d85c9c41aca1f4e1445f0335aecf8825e4
Matheus Costa <matheuscostadosantos@hotmail.com>
2021-07-12 15:02:39 -0300

    add footer e back-to-top

--- main.js
+++ main.js
@@ -51,9 +51,21 @@ scrollReveal.reveal(
   #about .image, #about .text,
   #services header, #services .card,
   #testimonials header, #testimonials .testimonials,
-  #contact .text, #contact .links
+  #contact .text, #contact .links,
+  footer .brand, footer .social
   `,
   {
     interval: 100,
   }
 );
+
+// Botão voltar para o topo
+
+const backToTopButton = document.querySelector(".back-to-top");
+window.addEventListener("scroll", function () {
+  if (window.scrollY >= 560) {
+    backToTopButton.classList.add("show");
+  } else {
+    backToTopButton.classList.remove("show");
+  }
+});
