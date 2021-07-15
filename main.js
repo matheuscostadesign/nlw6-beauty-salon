9e7ff645713f11dd3d0fd081b7a1c2292a0a939f
Matheus Costa <matheuscostadosantos@hotmail.com>
2021-07-14 22:59:29 -0300

    add favicons e finalizacao aula 05

--- main.js
+++ main.js
@@ -15,35 +15,6 @@ for (const link of links) {
   });
 }
 
-// Função - Alterar header
-function changeHeaderWhenScroll() {
-  const header = document.querySelector("#header");
-  const navHeight = header.offsetHeight;
-  if (window.scrollY >= navHeight) {
-    // adiciona se o scroll é maior que a altura do header
-    header.classList.add("scroll");
-  } else {
-    // remove se o scroll for menor que a altura do header
-    header.classList.remove("scroll");
-  }
-}
-
-// Função - Voltar para o topo
-function backtoTop() {
-  const backToTopButton = document.querySelector(".back-to-top");
-  if (window.scrollY >= 560) {
-    backToTopButton.classList.add("show");
-  } else {
-    backToTopButton.classList.remove("show");
-  }
-}
-
-// Chamada das funções pelo scroll
-window.addEventListener("scroll", function () {
-  changeHeaderWhenScroll();
-  backtoTop();
-});
-
 // Slider Swiper (Carrousel)
 const swiper = new Swiper(".swiper-container", {
   slidesPerView: 1,
@@ -80,3 +51,59 @@ scrollReveal.reveal(
     interval: 100,
   }
 );
+
+// Função - Alterar header
+const header = document.querySelector("#header");
+const navHeight = header.offsetHeight;
+function changeHeaderWhenScroll() {
+  if (window.scrollY >= navHeight) {
+    // adiciona se o scroll é maior que a altura do header
+    header.classList.add("scroll");
+  } else {
+    // remove se o scroll for menor que a altura do header
+    header.classList.remove("scroll");
+  }
+}
+
+// Função - Voltar para o topo
+const backToTopButton = document.querySelector(".back-to-top");
+function backtoTop() {
+  if (window.scrollY >= 560) {
+    backToTopButton.classList.add("show");
+  } else {
+    backToTopButton.classList.remove("show");
+  }
+}
+
+// Chamada das funções pelo scroll
+window.addEventListener("scroll", function () {
+  changeHeaderWhenScroll();
+  backtoTop();
+  activateMenuAtCurrentSection();
+});
+
+// Menu ativo conforme a sessão visível
+const sections = document.querySelectorAll("main section[id]");
+
+function activateMenuAtCurrentSection() {
+  const checkpoint = window.pageYOffset + (window.innerHeight / 8) * 4;
+
+  for (const section of sections) {
+    const sectionTop = section.offsetTop;
+    const sectionHeight = section.offsetHeight;
+    const sectionId = section.getAttribute("id");
+
+    const checkpointStart = checkpoint >= sectionTop;
+    const checkpointEnd = checkpoint <= sectionTop + sectionHeight;
+
+    if (checkpointStart && checkpointEnd) {
+      document
+        .querySelector("nav ul li a[href*=" + sectionId + "]")
+        .classList.add("active");
+    } else {
+      document
+        .querySelector("nav ul li a[href*=" + sectionId + "]")
+        .classList.remove("active");
+    }
+  }
+}
